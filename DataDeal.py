3bf6e042ac315f52f273ae1cf63db63c35fb9232
xiaosatianyu@126.com <xiaosatianyu@126.com>
2018-09-14 21:22:53 +0800

    movefor debug on local

--- DataDeal.py
+++ DataDeal.py
@@ -19,14 +19,22 @@ cur_dir = os.path.abspath(os.path.dirname(__file__))
 #install the coloredlogs
 coloredlogs.install(fmt=fmt)
 
+
+'''
+the default of total bitmap is 255 for each byte
+the default of trace bits if 0 for each byte
+'''
+
 class Collect():
-    def __init__(self, afl_work_dir, binary_path, ignore_ts, engine ,input_fix_len=300, from_file=True):
+    def __init__(self, afl_work_dir, binary_path, ignore_ts, engine , from_file=False):
         '''
         ignore_ts: ignore threshold, if the smaple number less than it, ignore
         input_fix_len : the fixed length of the input; if less, add 0; if more ,cut
         '''
         self.afl_work_dir   = afl_work_dir
         self.all_data_dir   = os.path.join(afl_work_dir, "data")
+
+        self.total_bitmap_path   = os.path.join(afl_work_dir, "fuzz_bitmap")
         
         self.ignore_ts      =ignore_ts
         
@@ -40,14 +48,59 @@ class Collect():
         self.engine    =  engine
         self.json_file_path = engine+"data.json"
 
-        self.input_fix_len = input_fix_len
-
         # all inputs it is huge 
         self.inputs_with_label = list() #each element is a tuple, which is the inputs and bitmap paths
 
+        # get the useful index from the total bitmap
+        self.useful_index = set() #save the non -1 index of the total bitmap, meaning these index are used
+        self._useful_index_from_total_bitmap()
+        
+        self.reduce_tail = "-reduce"
+        self.reduce_use_old = True
+
+
+
+    def save_reduce_trace_bitmap(self, reduce_file, reduce_content):
+       
+        #content = bytes()
+        #for val in reduce_content:
+        #    content += struct.pack('c', val)
+
+        with open(reduce_file, 'wb') as f:
+            f.write(reduce_content)
+        
+        with open(reduce_file, 'rb') as f:
+            check_content=f.read()
+        
+        # for check
+        for index in xrange( len(self.useful_index )):
+            if reduce_content[index] == check_content[index]:
+                continue
+            else:
+                l.info("there is some wrong")
+                exit(1)
+                break
+
+    def reduce_trace_bitmap(self, file):
+
+        if os.path.exists(file+self.reduce_tail) and  self.reduce_use_old:
+            return
+
+        with open(file, "rb") as f:
+            content = f.read()
+            content = struct.unpack('c'*len(content), content) ## return a tuple  B mean unsign
+
+        reduce_content= bytearray()
+
+        for index in self.useful_index:
+            if content[index] == b'\x00':
+                reduce_content.append( b'\x00' )
+            else:
+                reduce_content.append( b'\x01' )
+
+        self.save_reduce_trace_bitmap(file+self.reduce_tail, reduce_content)
 
     def collect_by_path(self):
-        
         if self.from_file: 
             if self.load_from_json():
                 l.info("load from file")
@@ -56,26 +109,47 @@ class Collect():
         l.info("begin to collect the input, wait for some time")
         for path_hash in os.listdir(self.all_data_dir):
             sole_data_dir = os.path.join(self.all_data_dir, path_hash)
-            
             path = Path(path_hash, sole_data_dir, self.ignore_ts)
-           
             # collect all input path
             input_paths = path.get_input_paths()
             bitmap_path = path.get_bitmap_path()
+
+            #reduce the trace bitmap
+            self.reduce_trace_bitmap(bitmap_path)
 
             for sole_input in input_paths:
                 self.inputs_with_label.append( (sole_input, bitmap_path) )
 
             # save the input number for each path
             self.path_num_dict.update({path_hash:path.inputs_num})
-
         # shuffle the list
         l.info("shuffle the inputs")
         random.shuffle(self.inputs_with_label)
-
         self.save_to_json()
-
         l.info("collect %d inputs with their bitmap!", len(self.inputs_with_label) )
+
+    '''
+    in the bitmap, each byte denote a transition
+    '''
+    def read_bitmap_content(self, file):
+        f = open(file, "rb")
+        content = f.read()
+        f.close()
+
+        # transform to a strtr
+        #content = struct.unpack('c'*len(content), content) ## it is a tuple  b mean sign, B mean unsign
+        
+        return content
+    
+    def _useful_index_from_total_bitmap(self):
+
+        content = self.read_bitmap_content(self.total_bitmap_path)  #the return type is tuple 
+
+        for index in xrange( len(content) ):
+            if not content[index]== '\xff':
+                self.useful_index.add(index)
+        l.info("usefull indes has %d length", len(self.useful_index))
+    
 
     def get_total_samples(self):
         l.info("there are %s samples", len(self.inputs_with_label))
@@ -92,10 +166,8 @@ class Collect():
         if not os.path.exists(self.json_file_path):
             l.warn("there is no %s", self.json_file_path)
             return False
-
         with open(self.json_file_path, 'r') as outfile:
             self.inputs_with_label  = json.load( outfile )
-
         return True
 
 
@@ -140,14 +212,15 @@ class Path():
 
 def main():
     engine="afl"
+    l.info("using the data from %s", engine)
+
     afl_work_dir = os.path.join(cur_dir, "output-"+engine)
     binary_path =  os.path.join(cur_dir, "benchmark/size")
     
     collect = Collect(afl_work_dir, binary_path, ignore_ts=30, engine=engine)
-    
+
     #1. collect the path of each input
     collect.collect_by_path()
-    collect.get_total_samples() 
 
 
 
--- neuzz.py
+++ neuzz.py
@@ -12,21 +12,24 @@ import logging
 from DataDeal import *
 import struct
 
+from sklearn.model_selection import train_test_split
+
 
 l=logging.getLogger("NEUZZ")
 l.setLevel("INFO") 
 fmt = "%(asctime)-15s %(filename)s:%(lineno)d %(process)d %(levelname)s %(message)s"
-#install the coloredlogs
 coloredlogs.install(fmt=fmt)
 
 cur_dir = os.path.abspath(os.path.dirname(__file__))
 
 #for model
 input_size  = 400
-output_size = 65536
+output_size = 1000
 strides = 3
-epochs = 1
-batch_size = 20
+epochs = 2 
+batch_size = 1
+use_rate =0.0005
+test_rate=0.25
 
 #for collect
 ignore_ts = 20
@@ -34,7 +37,7 @@ from_file = True  # data infor from
 
 
 class Nmodel():
-    def __init__(self, input_size, output_size, strides=1, batch_size=200, epochs=50):
+    def __init__(self, input_size, output_size, strides=1, batch_size=200, epochs=50, use_rate=1, test_rate=0.25):
         
         self.input_size  =  input_size 
         self.output_size = output_size 
@@ -42,32 +45,41 @@ class Nmodel():
         self.strides     = strides 
         self.epochs      = epochs
         self.batch_size  = batch_size
+       
+        self.test_rate = test_rate 
+        self.use_rate = use_rate # cut the input for some rate for test
 
-        self.inputs_with_label = None # the data path
+        self.all_inputs_with_label = None # the data path
         self.total_sample_number = 0  #all the sample size, including the train and test
+        
+        self.train_inputs_with_label = None # the train data
+        self.train_sample_number = 0  
+    
+        self.test_inputs_with_label = None # the test data 
+        self.test_sample_number = 0  
 
         self.model       = Sequential()
 
     def create_model(self):
-        self.model.add(Conv1D(64, 9, strides= self.strides,  padding="same", input_shape=( self.input_size,1)))
-        self.model.add( Activation("relu",name="relu1" ))
+        self.model.add( Conv1D(64, 9, strides=self.strides,  padding="same", input_shape=(self.input_size, 1) ) )
+        self.model.add( Activation("relu",name="relu1") )
        
-        self.model.add(Conv1D(64*2, 9, strides= self.strides,  padding="same"))
-        self.model.add( Activation("relu",name="relu2" ))
+        self.model.add( Conv1D(64*2, 9, strides=self.strides,  padding="same") ) 
+        self.model.add( Activation("relu",name="relu2") )
 
-        self.model.add(Conv1D(64*2*2, 9, activation='relu', strides= self.strides,  padding="same"))
-        self.model.add( Activation("relu",name="relu3" ))
+        self.model.add( Conv1D(64*2*2, 9, activation='relu', strides= self.strides, padding="same") )
+        self.model.add( Activation("relu",name="relu3") )
 
-        self.model.add(Dropout(0.25))
+        self.model.add( Dropout(0.25) )
 
-        self.model.add(Flatten())
-        self.model.add(Dense(self.output_size *1, activation='relu', name="full_connect_layer1" ))
+        self.model.add( Flatten() )
+        self.model.add( Dense(self.output_size*1, activation='relu', name="full_connect_layer1") )
         
         #self.model.add(Dropout(0.25))
         
         #self.model.add(Dense( self.output_size, name="full_connect_layer2"))
 
-        self.model.add(Activation("sigmoid",name="sigmoid" ))
+        self.model.add( Activation("sigmoid",name="sigmoid") )
 
         self.model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
@@ -79,16 +91,40 @@ class Nmodel():
         plot_model(self.model, to_file='model.png')
         
     def train_model(self):
-        self.model.fit_generator( self.generator_data_by_batch(), steps_per_epoch=self.total_sample_number/self.batch_size,
-                                  epochs = self.epochs, verbose =1 )
-        #self.model.fit(self.inputs_train, self.labels_train, batch_size=16, epochs=self.epochs)
+      
+        self.model.fit_generator( self.generator_train_data_by_batch(), 
+                                  steps_per_epoch = self.train_sample_number/self.batch_size, 
+                                  epochs = self.epochs, verbose=1,
+                                  validation_data = self.generator_test_data_by_batch(),
+                                  validation_steps = self.test_sample_number/self.batch_size 
+ )
+        
 
     def evaluate_model(self):
         score = self.model.evaluate(x_test, y_test, batch_size=self.batch_size)
-    
-    def set_all_data(self, inputs_with_label):
-        self.inputs_with_label = inputs_with_label
-        self.total_sample_number = len(self.inputs_with_label)
+   
+    def _split_data(self):
+
+        self.train_inputs_with_label, self.test_inputs_with_label = train_test_split( self.all_inputs_with_label, test_size = self.test_rate)
+
+        #deal with data
+        self.train_sample_number = len(self.train_inputs_with_label)/self.batch_size*self.batch_size
+        self.test_sample_number = len(self.test_inputs_with_label)/self.batch_size*self.batch_size
+        if self.test_sample_number == 0:
+            l.warn("there are too little data")
+            exit(0)
+
+        self.train_inputs_with_label = self.train_inputs_with_label[0: self.train_sample_number]
+        self.test_inputs_with_label = self.test_inputs_with_label[0: self.test_sample_number]
+        
+        
+
+    def set_all_data(self, all_inputs_with_label):
+        self.all_inputs_with_label = all_inputs_with_label[0 : int(len(all_inputs_with_label)*self.use_rate) ]
+        self.total_sample_number = len(self.all_inputs_with_label)
+
+        self._split_data()
+
 
     def _read_input_content(self, file):
         f = open(file, "rb")
@@ -103,14 +139,22 @@ class Nmodel():
 
         # transform
         content = struct.unpack('b'*len(content), content) ## it is a tuple
+
+        #normalization
         content = np.array(content).astype(np.float64)/255
         
         return content
 
-    def _read_bitmap_content(self, file):
+    def _read_reduced_bitmap_content(self, file):
         f = open(file, "rb")
         content = f.read()
         f.close()
+        
+        # control the length
+        if len(content) >= self.output_size:
+            content = content[0:self.output_size]
+        else:
+            content = content + b'\x00'*( self.output_size - len(content)) # the value is -128  to 127 , using b not B 
 
         # transform
         content = struct.unpack('b'*len(content), content) ## it is a tuple
@@ -127,54 +171,73 @@ class Nmodel():
         return content
 
     
-    def read_samples_by_size(self, size=10 , start_index=0):
+    def read_samples_by_size(self, size=10 , start_index=0, train = True):
         '''
         start_index : fromt start_index to start_index+size
         '''
-        
         inputs_data = np.ones( (1, self.input_size ,1) )
         labels_data = np.ones( (1, self.output_size ) )
         
         for i in xrange(size):
-            input_path = self.inputs_with_label[start_index +i][0]
-            bitmap_path = self.inputs_with_label[start_index +i][1]
+            if train:
+                #l.info("gest index  %d", start_index+i )
+                input_path  = self.train_inputs_with_label[start_index +i][0]
+                bitmap_path = self.train_inputs_with_label[start_index +i][1]
+            else:
+                input_path = self.test_inputs_with_label[start_index +i][0]
+                bitmap_path = self.test_inputs_with_label[start_index +i][1]
    
             input_content = self._read_input_content(input_path)
-            input_content = np.reshape( input_content , (1, len(input_content),1) )
+            input_content = np.reshape( input_content , (1, len(input_content), 1) )
             inputs_data = np.append( inputs_data, input_content, axis=0) 
 
-            bitmap_content = self._read_bitmap_content(bitmap_path)
+            bitmap_content = self._read_reduced_bitmap_content(bitmap_path)
             bitmap_content = np.reshape( bitmap_content , (1, len(bitmap_content)) )
             labels_data = np.append( labels_data, bitmap_content, axis=0)
 
         inputs_data = np.delete(inputs_data, 0, axis=0)
         labels_data = np.delete(labels_data, 0, axis=0)
         
-        #l.info("get the data from %d to %d", start_index, start_index +size )
+        #l.info("the length of train is %d", self.train_sample_number)
+        #l.info("get the data from [%d to %d)", start_index, start_index +size )
         #l.info("inputs_data shape %s", inputs_data.shape)
         #l.info("labels_data shape %s", labels_data.shape)
+        #l.info("\n")
         
         return (inputs_data, labels_data)
-
-
-    def generator_data_by_batch(self):
+    
+    
+    def generator_train_data_by_batch(self):
         '''
         it is the generator for the fit_generator function
         '''
-        start_index = 0
         
+        start_index = 0
         while True:
-            inputs_data, labels_data = self.read_samples_by_size( size = self.batch_size, start_index = start_index )
+            start_index %= self.train_sample_number
+            inputs_data, labels_data = self.read_samples_by_size( size = self.batch_size, start_index = start_index, train= True )
             start_index += batch_size
+            #l.info("generator train data from [%d, to %d)", start_index, start_index + self.batch_size)
             yield (inputs_data, labels_data)
-
-
+    
+    def generator_test_data_by_batch(self):
+        '''
+        it is the generator for the fit_generator function
+        '''
+        
+        start_index = 0
+        while True:
+            start_index %= self.test_sample_number
+            inputs_data, labels_data = self.read_samples_by_size( size = self.batch_size, start_index = start_index, train=False)
+            start_index += batch_size
+            #l.info("generator test data from [%d, to %d)", start_index, start_index + self.batch_size)
+            yield (inputs_data, labels_data)
 
 
 def test_part_data(  ):
     #data source
-    engine = "fair" 
-    #engine = "afl"
+    #engine = "fair" 
+    engine = "afl"
 
     afl_work_dir = os.path.join(cur_dir, "output-"+engine )
     binary_path =  os.path.join(cur_dir, "benchmark/size")
@@ -190,19 +253,19 @@ def test_part_data(  ):
 
     #2.init the model
     nmodel = Nmodel( input_size = input_size, output_size = output_size, 
-                    strides = strides, epochs = epochs, batch_size = batch_size  )
+                    strides = strides, epochs = epochs, batch_size = batch_size , use_rate =use_rate, test_rate=test_rate )
 
     nmodel.create_model()
     
     nmodel.get_model_net()
-    exit(0)    
+    #exit(0)    
     
     #2. read the content of each input
-    inputs_with_label = collect.get_data()
-    nmodel.set_all_data( inputs_with_label)
+    all_inputs_with_label = collect.get_data()
+    nmodel.set_all_data( all_inputs_with_label)
 
     # for test
-    nmodel.read_samples_by_size(1)
+    nmodel.read_samples_by_size(10)
     #exit(0)
 
     # train the model
