c5df8cc246fd364e80d6c71d1ba23cbcd7ff2c21
xiaosatianyu@126.com <xiaosatianyu@126.com>
2018-09-16 21:28:21 +0800

    modify the afl script

--- run_afl.sh
+++ run_afl.sh
@@ -1,4 +1,26 @@
-Output_dir="./output-afl"
-rm -rf $Output_dir
-mkdir -p $Output_dir
-  ./afl/afl-fuzz -i ./seed -o $Output_dir  -Q ./benchmark/size @@
+if [ ! $# == 2 ]; then
+    echo "Usage: $0 engine binary"
+    exit
+fi
+
+Target=$2
+Engine=$1
+PWD=`pwd`
+
+if [ "$Engine"x = "afl"x ]; then
+	AFL_HOME=$PWD/afl # afl
+	OUTPUT=$PWD/output-afl-$Target
+	echo "using afl"
+elif [ "$Engine"x = "fair"x ]; then
+	AFL_HOME=$PWD/afl-rb #aflnb
+	OUTPUT=$PWD/output-fair-$Target
+	echo "using aflnb"
+else
+    echo "enging wrong"
+    exit
+fi
+
+rm -rf $OUTPUT
+mkdir -p $OUTPUT
+
+$AFL_HOME/afl-fuzz -i $PWD/seed -o $OUTPUT  ./benchmark/$Target
--- run_fair.sh
+++
@@ -1,4 +0,0 @@
-Output_dir="./output-fair"
-rm -rf $Output_dir
-mkdir -p $Output_dir
-  ./afl-rb/afl-fuzz -i ./seed -o $Output_dir  -Q ./benchmark/size @@
